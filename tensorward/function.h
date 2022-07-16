495418b84fdf0f46e2e81a3ef52a891ebd277904
hotsuyuki <cube.0529@gmail.com>
2022-07-16 18:43:22 +0900

    Add header file aggregation in function.h

--- tensorward/function.h
+++ tensorward/function.h
@@ -1,6 +1,12 @@
 #pragma once
 
 // Header file aggregation for users.
+#include "tensorward/function/broadcast_to.h"
 #include "tensorward/function/exp.h"
+#include "tensorward/function/matmul.h"
 #include "tensorward/function/pow.h"
+#include "tensorward/function/reshape.h"
 #include "tensorward/function/square.h"
+#include "tensorward/function/sum.h"
+#include "tensorward/function/sum_to.h"
+#include "tensorward/function/transpose.h"
