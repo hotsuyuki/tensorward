ad96b56d9255db61343e6fba25cd4c266d184397
hotsuyuki <cube.0529@gmail.com>
2022-07-23 19:02:44 +0900

    Include "parameter.h" in the header file aggregation "core.h"

--- tensorward/core.h
+++ tensorward/core.h
@@ -3,6 +3,7 @@
 // Header file aggregation for users.
 #include "tensorward/core/config.h"
 #include "tensorward/core/function.h"
+#include "tensorward/core/parameter.h"
 #include "tensorward/core/tensor.h"
 #include "tensorward/core/operator/add.h"
 #include "tensorward/core/operator/div.h"
