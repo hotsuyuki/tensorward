ddaa306f5a42234e9787317c1ca7eeb527b61ba3
hotsuyuki <cube.0529@gmail.com>
2022-07-24 20:29:53 +0900

    Use `Model` class in the example code for non-linear-regression
    In order to simplify the management of the parameters,
    e.g. gradient clearing, parameter updating, etc.

--- example/4_non_linear_regression/main.cc
+++ example/4_non_linear_regression/main.cc
@@ -7,16 +7,18 @@
 
 #include "tensorward/core.h"
 #include "tensorward/function.h"
+#include "tensorward/model.h"
 
 #define DEBUG_PRINT(var) std::cout << #var << " = " << var << std::endl;
 
 namespace tw = tensorward::core;
 namespace F = tensorward::function;
+namespace M = tensorward::model;
 
 int main(int argc, char* argv[]) {
   constexpr int kDataSize = 100;
   constexpr int kInSize = 1;
-  constexpr int kHideSize = 10;
+  constexpr int kHiddenSize = 10;
   constexpr int kOutSize = 1;
 
   constexpr float kLearningRate = 0.2;
@@ -31,34 +33,24 @@ int main(int argc, char* argv[]) {
   const tw::TensorSharedPtr x_ptr = tw::AsTensorSharedPtr(x_data, "x");
   const tw::TensorSharedPtr y_ptr = tw::AsTensorSharedPtr(y_data, "y");
 
-  // Parameters
-  const tw::TensorSharedPtr W1_ptr = tw::AsTensorSharedPtr(0.01 * xt::random::rand<float>({kInSize, kHideSize}), "W1");
-  const tw::TensorSharedPtr b1_ptr = tw::AsTensorSharedPtr(xt::zeros<float>({kHideSize}), "b1");
-  const tw::TensorSharedPtr W2_ptr = tw::AsTensorSharedPtr(0.01 * xt::random::rand<float>({kHideSize, kOutSize}), "W2");
-  const tw::TensorSharedPtr b2_ptr = tw::AsTensorSharedPtr(xt::zeros<float>({kOutSize}), "b2");
+  // Model
+  M::MultiLayerPerceptron multi_layer_perceptron_model({kHiddenSize, kOutSize}, F::sigmoid_lambda);
 
   for (std::size_t i = 0; i < kIterations; ++i) {
     // Prediction
-    tw::TensorSharedPtr y_pred_ptr;
-    y_pred_ptr = F::linear(x_ptr, W1_ptr, b1_ptr);
-    y_pred_ptr = F::sigmoid(y_pred_ptr);
-    y_pred_ptr = F::linear(y_pred_ptr, W2_ptr, b2_ptr);
+    const tw::TensorSharedPtr y_pred_ptr = multi_layer_perceptron_model.Predict({x_ptr})[0];
 
     // Loss
     const tw::TensorSharedPtr loss_ptr = F::mean_squared_error(y_ptr, y_pred_ptr);
 
     // Backpropagation
-    W1_ptr->ClearGrad();
-    b1_ptr->ClearGrad();
-    W2_ptr->ClearGrad();
-    b2_ptr->ClearGrad();
+    multi_layer_perceptron_model.ClearGrads();
     loss_ptr->Backpropagation();
 
-    // Parameter updating
-    W1_ptr->SeData(W1_ptr->data() - kLearningRate * W1_ptr->grad());
-    b1_ptr->SeData(b1_ptr->data() - kLearningRate * b1_ptr->grad());
-    W2_ptr->SeData(W2_ptr->data() - kLearningRate * W2_ptr->grad());
-    b2_ptr->SeData(b2_ptr->data() - kLearningRate * b2_ptr->grad());
+    // Parameter update
+    for (const auto& param_ptr : multi_layer_perceptron_model.GetParamPtrs()) {
+      param_ptr->SeData(param_ptr->data() - kLearningRate * param_ptr->grad());
+    }
 
     if (i % (kIterations / 10) == 0) {
       DEBUG_PRINT(i);
@@ -69,16 +61,31 @@ int main(int argc, char* argv[]) {
 
   std::cout << "------------------------------------" << std::endl << std::endl;
 
-  DEBUG_PRINT(W1_ptr);
+  for (const auto& param_ptr : multi_layer_perceptron_model.GetParamPtrs()) {
+    DEBUG_PRINT(param_ptr);
+    std::cout << std::endl;
+  }
+
+  std::cout << "xs = [";
+  for (const auto& x : x_ptr->data()) {
+    std::cout << x << ", ";
+  }
+  std::cout << "]" << std::endl;
   std::cout << std::endl;
 
-  DEBUG_PRINT(b1_ptr);
+  std::cout << "ys = [";
+  for (const auto& y : y_ptr->data()) {
+    std::cout << y << ", ";
+  }
+  std::cout << "]" << std::endl;
   std::cout << std::endl;
 
-  DEBUG_PRINT(W2_ptr);
-  std::cout << std::endl;
-
-  DEBUG_PRINT(b2_ptr);
+  const tw::TensorSharedPtr y_pred_ptr = multi_layer_perceptron_model.Predict({x_ptr})[0];
+  std::cout << "ys_pred = [";
+  for (const auto& y_pred : y_pred_ptr->data()) {
+    std::cout << y_pred << ", ";
+  }
+  std::cout << "]" << std::endl;
   std::cout << std::endl;
 
   return EXIT_SUCCESS;
