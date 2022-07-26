8ffc25ddd861537ef8590a5f3614b41948a8789a
hotsuyuki <cube.0529@gmail.com>
2022-07-26 20:36:46 +0900

    Add `StochasticGradientDescent` class, which is derived from `Optimizer` class

---
+++ tensorward/optimizer/stochastic_gradient_descent.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "tensorward/core/optimizer.h"
+#include "tensorward/core/parameter.h"
+
+namespace tensorward::optimizer {
+
+class StochasticGradientDescent : public core::Optimizer {
+ public:
+  StochasticGradientDescent(const float learning_rate) : learning_rate_(learning_rate) {}
+
+  ~StochasticGradientDescent() {}
+
+  void UpdateSingleParameter(const core::ParameterSharedPtr param_ptr) const override {
+    param_ptr->SeData(param_ptr->data() - learning_rate_ * param_ptr->grad());
+  }
+
+  const float learning_rate() const { return learning_rate_; }
+
+ private:
+  float learning_rate_;
+};
+
+}  // tensorward::optimizer
---
+++ tensorward/optimizer/test/stochastic_gradient_descent_test.cc
@@ -0,0 +1,45 @@
+#include "tensorward/optimizer/stochastic_gradient_descent.h"
+
+#include <gtest/gtest.h>
+#include <xtensor/xarray.hpp>
+#include <xtensor/xrandom.hpp>
+
+namespace tensorward::optimizer {
+
+namespace {
+
+constexpr int kInSize = 2;
+constexpr int kOutSize = 3;
+constexpr float kLearningRate = 0.01;
+
+}  // namespace
+
+class StochasticGradientDescentTest : public ::testing::Test {
+ protected:
+  StochasticGradientDescentTest()
+      : parameter_ptr_(core::AsParameterSharedPtr(xt::random::rand<float>({kInSize, kOutSize}))),
+        stochastic_gradient_descent_optimizer_(kLearningRate) {}
+
+  const core::ParameterSharedPtr parameter_ptr_;
+  const optimizer::StochasticGradientDescent stochastic_gradient_descent_optimizer_;
+};
+
+TEST_F(StochasticGradientDescentTest, UpdateSingleParameterTest) {
+  // Sets a pseudo gradient.
+  const xt::xarray<float> dL_dp = xt::ones_like(parameter_ptr_->data());
+  parameter_ptr_->SetGradOpt(dL_dp);
+
+  // Sets an expected parameter data according to the Stochastic Gradient Decent equation.
+  // p <--- p - lr * dL_dp
+  const xt::xarray<float> expected_parameter_data = parameter_ptr_->data() - kLearningRate * dL_dp;
+
+  stochastic_gradient_descent_optimizer_.UpdateSingleParameter(parameter_ptr_);
+
+  // Prepares actual parameter datas.
+  const xt::xarray<float> actual_parameter_data = parameter_ptr_->data();
+
+  // Checks that the updated parameters are correct.
+  EXPECT_EQ(actual_parameter_data, expected_parameter_data);
+}
+
+}  // namespace tensorward::optimizer
