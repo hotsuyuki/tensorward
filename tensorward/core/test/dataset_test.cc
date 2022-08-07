a495f5626c4acd252b503dea151cf2c40f237b97
hotsuyuki <cube.0529@gmail.com>
2022-08-07 21:18:50 +0900

    Fix typos

--- tensorward/core/test/dataset_test.cc
+++ tensorward/core/test/dataset_test.cc
@@ -20,20 +20,20 @@ constexpr bool kIsTrainingMode = true;
 class DatasetTest : public ::testing::Test {
  protected:
   DatasetTest()
-      : make_it_half_lambda_([](const xt::xarray<float>& input_data) { return input_data / 2.0; }),
-        make_it_onehot_lambda_([](const xt::xarray<float>& input_data) {
+      : transform_to_half_lambda_([](const xt::xarray<float>& input_data) { return input_data / 2.0; }),
+        transform_to_onehot_lambda_([](const xt::xarray<float>& input_data) {
           const std::size_t class_index = static_cast<std::size_t>(input_data(0));
           assert(class_index < kClassSize);
           xt::xarray<float> output_data = xt::zeros<float>({kClassSize});
           xt::view(output_data, class_index) = 1.0;
           return output_data;
         }),
-        data_transform_lambdas_({make_it_half_lambda_}),
-        label_transform_lambdas_({make_it_onehot_lambda_}),
+        data_transform_lambdas_({transform_to_half_lambda_}),
+        label_transform_lambdas_({transform_to_onehot_lambda_}),
         spiral_dataset_(kIsTrainingMode, data_transform_lambdas_, label_transform_lambdas_) {}
 
-  const TransformLambda make_it_half_lambda_;
-  const TransformLambda make_it_onehot_lambda_;
+  const TransformLambda transform_to_half_lambda_;
+  const TransformLambda transform_to_onehot_lambda_;
   const std::vector<TransformLambda> data_transform_lambdas_;
   const std::vector<TransformLambda> label_transform_lambdas_;
   const dataset::Spiral spiral_dataset_;
@@ -59,11 +59,11 @@ TEST_F(DatasetTest, AtTest) {
     xt::xarray<float> ith_data(xt::view(spiral_dataset_.data(), i));
     xt::xarray<float> ith_label(xt::view(spiral_dataset_.label(), i));
 
-    // Prepares the expected i-th data, and applies the "make-it-half" transform to it.
+    // Prepares the expected i-th data, and applies the "transform-to-half" transform to it.
     xt::xarray<float> expected_ith_data = ith_data;
     expected_ith_data = expected_ith_data / 2.0;
 
-    // Prepares the expected i-th label, and applies the "make-it-onehot" transform to it.
+    // Prepares the expected i-th label, and applies the "transform-to-onehot" transform to it.
     const std::size_t class_index = static_cast<std::size_t>(ith_label(0));
     assert(class_index < kClassSize);
     xt::xarray<float> expected_ith_label = xt::zeros<float>({kClassSize});
